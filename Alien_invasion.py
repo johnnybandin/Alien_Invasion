8406a5de06901b9d8fa1bb92630a3a2671d61b93
johnnybandin <63764377+johnnybandin@users.noreply.github.com>
2020-04-18 17:35:44 -0400

    Add files via upload

---
+++ Alien_invasion.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+from ship import Ship
+from settings import Settings
+class AlienInvasion:
+	"""Overall class to manage game assets and behavior"""
+
+	def __init__(self):
+		"""Initialize the game, and create game resources"""
+		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+
+		self.ship = Ship(self)
+
+		# Set the background color
+		self.bg_color = (3, 252, 252)
+	def run_game(self):
+		"""Start the main loop for the game"""
+		while True:
+			#Watch for keyboard and mouse movements
+			self._check_events()
+			self.ship.update()
+			self._update_screen()
+
+			# Make the most recently drawn screen visible.
+			pygame.display.flip()
+	def _check_events(self):
+		"""Respond to keypresses and mouse movements"""
+		for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+				elif event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_RIGHT:
+						self.ship.moving_right = True
+					elif event.key == pygame.K_LEFT:
+						self.ship.moving_left = True
+				elif event.type == pygame.KEYUP:
+					if event.key == pygame.K_RIGHT:
+						self.ship.moving_right = False
+					elif event.key == pygame.K_LEFT:
+						self.ship.moving_left = False
+	def _update_screen(self):
+		"""update images on the screen, and flip to the new screen."""
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+
+		# Make the most recently drawn screen visible.
+		pygame.display.flip()
+if __name__ == '__main__':
+	# Make a game instance, and run the game
+	ai = AlienInvasion()
+	ai.run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+# The settings for the game alien invasion
+class Settings:
+	"""A class to store all the settings"""
+
+	def __init__(self):
+		"""Initialize the game's setting"""
+		# screen settings
+		self.screen_width = 1800
+		self.screen_height = 800
+		self.bg_color = (3, 252, 252)
